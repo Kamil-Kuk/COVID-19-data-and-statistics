1be2c540407a5c5c8f27c8256d15709dd6e943fa
kamilK <kamil.kukowski@gmail.com>
2020-12-30 11:40:41 +0100

    updated Country.java

--- src/main/java/DB/COVID19Runner.java
+++ src/main/java/DB/COVID19Runner.java
@@ -1,7 +1,56 @@
 package DB;
 
+import DB.entities.Country;
+import DB.entities.adapters.CountryAdapter;
+import main.CsvRead.CsvBean;
+import main.CsvRead.CsvBeanOWID;
+import main.CsvRead.CsvRead;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Iterator;
+import java.util.List;
+
 public class COVID19Runner {
     public static void main(String[] args) {
 
+        COVID19DAOImpl<Country> daoCountries = new COVID19DAOImpl<>();
+        daoCountries.openConnection();
+
+        CsvRead csvRead = new CsvRead(new CsvBeanOWID());
+        List<CsvBeanOWID> result = csvRead.getBeanFromCSV();
+
+        CsvBeanOWID bean = new CsvBeanOWID();
+        bean.setISO_code("POL");
+        bean.setContinent("Europe");
+        bean.setLocation("Poland");
+        bean.setPopulation("38000000.0");
+
+        Country entity = new Country();
+        entity.set(bean);
+        System.out.println(entity.getClass());
+
+        daoCountries.save(entity);
+//        CountryAdapter countryEntry =  new CountryAdapter((CsvBeanOWID) beans.get(0));
+//        countryEntry.setISO_code();
+//        daoCountries.save(countryEntry);
+
+//        for(CsvBean bean : beans) {
+//            countryEntry = new CountryAdapter((CsvBeanOWID) bean);
+//            countryEntry.setISO_code();
+//            countryEntry.setContinent();
+//            countryEntry.setName();
+//            countryEntry.setPopulation();
+//            daoCountries.save(countryEntry);
+//        }
+
+        daoCountries.closeConnection();
+
+//        CsvBeanOWID bean = new CsvBeanOWID();
+//        CountryAdapter entity1 = new CountryAdapter(bean);
+//        bean.setISO_code("");
+//        entity1.setISO_code();
+//        System.out.println(entity1.getISO_code());
     }
 }
--- src/main/java/DB/entities/Country.java
+++ src/main/java/DB/entities/Country.java
@@ -2,6 +2,7 @@ package DB.entities;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import main.CsvRead.CsvBeanOWID;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -9,7 +10,6 @@ import java.util.Set;
 
 @Entity
 @Getter
-@NoArgsConstructor
 public class Country {
 
     @Id
@@ -28,6 +28,9 @@ public class Country {
     @OneToMany(mappedBy = "country", cascade = CascadeType.ALL)
     private Set<OWID> recordsOWID = new HashSet<>();
 
+    public Country() {
+    }
+
     public void setISO_code(String ISO_code) {
         this.ISO_code = ISO_code;
     }
@@ -42,6 +45,32 @@ public class Country {
 
     public void setPopulation(Integer population) {
         this.population = population;
+    }
+
+    private void setISO_codeFromBean(CsvBeanOWID bean) {
+        if(!bean.getISO_code().isEmpty()) setISO_code(bean.getISO_code());
+    }
+
+    private void setContinentFromBean(CsvBeanOWID bean) {
+        if(!bean.getContinent().isEmpty()) setContinent(bean.getContinent());
+    }
+
+    private void setNameFromBean(CsvBeanOWID bean) {
+        if(!bean.getLocation().isEmpty()) setName(bean.getLocation());
+    }
+
+    private void setPopulationFromBean(CsvBeanOWID bean) {
+        if(!bean.getPopulation().isEmpty()) {
+            Double populationDouble = Double.parseDouble(bean.getPopulation());
+            setPopulation(populationDouble.intValue());
+        }
+    }
+
+    public void set(CsvBeanOWID bean){
+        setISO_codeFromBean(bean);
+        setContinentFromBean(bean);
+        setNameFromBean(bean);
+        setPopulationFromBean(bean);
     }
 
     public void addRecordOWID(OWID record) { this.recordsOWID.add(record); }
